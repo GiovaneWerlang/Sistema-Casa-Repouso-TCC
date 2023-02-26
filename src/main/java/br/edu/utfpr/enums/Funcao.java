d4965c7f7ce541f77a6c36ae23fc35bff00821e4
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-02-26 17:53:32 -0300

    Funcao

---
+++ src/main/java/br/edu/utfpr/enums/Funcao.java
@@ -0,0 +1,19 @@
+package br.edu.utfpr.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Funcao {
+
+    CUIDADOR("Cuidador"),
+    ENFERMEIRO("Enfermeiro"),
+    FUNCIONARIO("Funcionário"),
+    MEDICO("Médico"),
+    VOLUTARIO("Volutário");
+
+    private String descricao;
+
+    Funcao(String descricao){
+        this.descricao = descricao;
+    }
+}
