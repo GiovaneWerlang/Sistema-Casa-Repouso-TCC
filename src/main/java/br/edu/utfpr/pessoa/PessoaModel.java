6439778a597ec59ed9c2c4439699dc2551cbc869
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-02-26 17:53:45 -0300

    Pessoa

---
+++ src/main/java/br/edu/utfpr/pessoa/PessoaModel.java
@@ -0,0 +1,34 @@
+package br.edu.utfpr.pessoa;
+
+import br.edu.utfpr.endereco.EnderecoModel;
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@MappedSuperclass
+public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "idade")
+    private int idade;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idendereco")
+    private EnderecoModel endereco;
+}
