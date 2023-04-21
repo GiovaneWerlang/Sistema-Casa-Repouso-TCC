a4c1ebf6ecfc5523124e6aa7fddaa9a973ba4969
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-04-21 14:31:46 -0300

    Especialidade Service

--- src/main/java/br/edu/utfpr/especialidade/EspecialidadeResource.java
+++ src/main/java/br/edu/utfpr/especialidade/EspecialidadeResource.java
@@ -1,18 +1,13 @@
 package br.edu.utfpr.especialidade;
 
-import br.edu.utfpr.erro.ResponseError;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
-import java.util.Set;
 
 @Path("/especialidade")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -20,84 +15,42 @@ import java.util.Set;
 @Tag(name = "Especialidade")
 public class EspecialidadeResource {
 
-    private EspecialidadeRepository repository;
-    private Validator validator;
+    private EspecialidadeService service;
 
     @Inject
-    public EspecialidadeResource(EspecialidadeRepository repository, Validator validator){
-        this.repository = repository;
-        this.validator = validator;
+    public EspecialidadeResource(EspecialidadeService service){
+        this.service = service;
     }
 
     @Operation(summary = "Retorna todas")
     @GET
     public Response getAll(){
-        List<EspecialidadeModel> lista = repository.listAll();
-        if(lista.isEmpty()){
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-        return Response.ok(lista).build();
+        return service.getAll();
     }
 
     @GET
     @Path("{id}")
     public Response getEspecialidadeById(@PathParam("id") long id){
-        EspecialidadeModel model = repository.findById(id);
-        if(model != null){
-            return Response.ok(model).build();
-        }
-
-        return Response.status(Response.Status.NOT_FOUND).build();
+       return service.findById(id);
     }
     @POST
     @Transactional
     public Response addEspecialidade(EspecialidadeDTO especialidadeDTO){
-        Set<ConstraintViolation<EspecialidadeDTO>> violations = validator.validate(especialidadeDTO);
-        if(!violations.isEmpty()){
-            return ResponseError.createFromViolations(violations).returnWithStatusCode(422);
-        }
-
-        EspecialidadeModel model = new EspecialidadeModel();
-        model.setNome(especialidadeDTO.getNome());
-
-        try{
-            repository.persist(model);
-        }catch (Exception ex){
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-
-        return Response.status( Response.Status.CREATED.getStatusCode(),model.toString()).build();
+       return service.add(especialidadeDTO);
     }
 
     @PUT
     @Path("{id}")
     @Transactional
     public Response updateEspecialidade(@PathParam("id") long id, EspecialidadeDTO especialidadeDTO){
-        Set<ConstraintViolation<EspecialidadeDTO>> violations = validator.validate(especialidadeDTO);
-        if(!violations.isEmpty()){
-            return ResponseError.createFromViolations(violations).returnWithStatusCode(422);
-        }
-
-        EspecialidadeModel model = repository.findById(id);
-        if(model != null){
-            model.setNome(especialidadeDTO.getNome());
-
-            return Response.status(201, model.toString()).build();
-        }
-
-        return Response.status(Response.Status.NOT_FOUND).build();
+       return service.update(id, especialidadeDTO);
     }
 
     @DELETE
     @Path("{id}")
     @Transactional
     public Response deleteEspecialidade(@PathParam("id") long id){
-        EspecialidadeModel model = repository.findById(id);
-        if(model != null){
-            repository.delete(model);
-            return Response.ok(model).build();
-        }
-
-        return Response.status(Response.Status.NOT_FOUND).build();
+        return service.delete(id);
     }
+
 }
---
+++ src/main/java/br/edu/utfpr/especialidade/EspecialidadeService.java
@@ -0,0 +1,85 @@
+package br.edu.utfpr.especialidade;
+
+import br.edu.utfpr.erro.ResponseError;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Set;
+
+@ApplicationScoped
+public class EspecialidadeService {
+
+    private EspecialidadeRepository repository;
+    private Validator validator;
+
+    @Inject
+    public EspecialidadeService(EspecialidadeRepository repository, Validator validator) {
+        this.repository = repository;
+        this.validator = validator;
+    }
+
+    public Response getAll(){
+        List<EspecialidadeModel> lista = repository.listAll();
+        if(lista.isEmpty()){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(lista).build();
+    }
+
+    public Response findById(long id){
+        EspecialidadeModel model = repository.findById(id);
+        if(model != null){
+            return Response.ok(model).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    public Response add(EspecialidadeDTO especialidadeDTO){
+        Set<ConstraintViolation<EspecialidadeDTO>> violations = validator.validate(especialidadeDTO);
+        if(!violations.isEmpty()){
+            return ResponseError.createFromViolations(violations).returnWithStatusCode(422);
+        }
+
+        EspecialidadeModel model = new EspecialidadeModel();
+        model.setNome(especialidadeDTO.getNome());
+
+        try{
+            repository.persist(model);
+        }catch (Exception ex){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+
+        return Response.status( Response.Status.CREATED.getStatusCode(),model.toString()).build();
+    }
+
+    public Response update(long id, EspecialidadeDTO especialidadeDTO){
+        Set<ConstraintViolation<EspecialidadeDTO>> violations = validator.validate(especialidadeDTO);
+        if(!violations.isEmpty()){
+            return ResponseError.createFromViolations(violations).returnWithStatusCode(422);
+        }
+
+        EspecialidadeModel model = repository.findById(id);
+        if(model != null){
+            model.setNome(especialidadeDTO.getNome());
+
+            return Response.status(201, model.toString()).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    public Response delete(long id){
+        EspecialidadeModel model = repository.findById(id);
+        if(model != null){
+            repository.delete(model);
+            return Response.ok(model).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+}
