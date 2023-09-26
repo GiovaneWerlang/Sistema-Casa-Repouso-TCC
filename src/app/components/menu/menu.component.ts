53b46cd81a7db38c079541ffca6bc2913a045bbb
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-09-26 15:23:36 -0300

    update menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,24 +8,45 @@ import { Component } from '@angular/core';
 export class MenuComponent {
   items = [
     {
-        label: 'Home',
-        icon: 'pi pi-home',
-        url: 'home'
-    },
-    {
-        label: 'Delete',
-        icon: 'pi pi-fw pi-trash',
-        command: () => {
-          this.update();
-      }
-    },{
+      label: 'Home',
+      icon: 'pi pi-home',
+      url: 'home'
+    }, {
+      label: 'Atividades Lúdicas',
+      icon: 'pi pi-link',
+      url: 'atividadeludica/listar'
+    }, {
+      label: 'Consulta',
+      icon: 'pi pi-link',
+      url: 'consulta/listar'
+    }, {
+      label: 'Entradas e Saídas',
+      icon: 'pi pi-link',
+      url: 'entradasaida/listar'
+    }, {
       label: 'Especialidade',
       icon: 'pi pi-link',
       url: 'especialidade/listar'
+    }, {
+      label: 'Exame',
+      icon: 'pi pi-link',
+      url: 'exame/listar'
+    }, {
+      label: 'Medicamento',
+      icon: 'pi pi-link',
+      url: 'medicamento/listar'
+    }, {
+      label: 'Profissional',
+      icon: 'pi pi-link',
+      url: 'profissional/listar'
+    }, {
+      label: 'Residente',
+      icon: 'pi pi-link',
+      url: 'residente/listar'
+    }, {
+      label: 'Usuário',
+      icon: 'pi pi-link',
+      url: 'usuario/listar'
     }
-];
-
-update(){
-  console.log('update')
-}
+  ];
 }
