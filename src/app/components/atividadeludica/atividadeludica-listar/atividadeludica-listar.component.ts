209f84273270945ec40c84f284bc53ed74a8e666
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-09-29 20:28:30 -0300

    melhoria sort atividade ludica

--- src/app/components/atividadeludica/atividadeludica-listar/atividadeludica-listar.component.ts
+++ src/app/components/atividadeludica/atividadeludica-listar/atividadeludica-listar.component.ts
@@ -10,64 +10,65 @@ import { Situacoes } from 'src/app/shared/situacoes/situacoes';
   selector: 'app-atividadeludica',
   templateUrl: './atividadeludica-listar.component.html',
   styleUrls: ['./atividadeludica-listar.component.css'],
-  providers: [  { provide: CrudService, useExisting: AtividadeLudicaService}]
+  providers: [{ provide: CrudService, useExisting: AtividadeLudicaService }]
 })
 export class AtividadeludicaListarComponent {
 
   public items: AtividadeLudica[] = [];
 
-  first:number = 0;
-  rows:number = 10;
-  total:number = 0;
+  first: number = 0;
+  rows: number = 10;
+  total: number = 0;
 
-  page:number = 0;
+  page: number = 0;
+  sort: string = "id";
+  asc: boolean = true;
 
-  situacoes:LabelValue[] = Situacoes;
+  situacoes: LabelValue[] = Situacoes;
 
   constructor(
-    private service:CrudService<AtividadeLudica>,
+    private service: CrudService<AtividadeLudica>,
     private router: Router
-    ) {
-      //this.carregarLista(this.first, this.rows);
-    }
+  ) { }
 
-  novo(){
+  novo() {
     this.router.navigate([`/${this.service.getUrl()}/cadastrar`]);
   }
 
   ngOnInit(): void {
   }
 
-  delete(id:any){
-    this.service.delete(id).subscribe((res)=>{
+  delete(id: any) {
+    this.service.delete(id).subscribe((res) => {
       this.carregarLista(0, this.rows);
     })
   }
 
-  edit(id:number){
+  edit(id: number) {
     this.router.navigate([`/${this.service.getUrl()}/editar/${id}`]);
   }
 
-  carregarLista(page:number, size: number):void{
-    this.service.pagesort(page, size,"id",true).subscribe((page:any)=>{     
+  carregarLista(page: number, size: number): void {
+    this.service.pagesort(page, size, this.sort, this.asc).subscribe((page: any) => {
       this.items = page.lista;
       this.total = page.total;
     })
   }
 
-  onPageChange(event:any){
+  onPageChange(event: any) {
     this.rows = event.rows;
     this.page = event.page;
     this.carregarLista(event.page, event.rows);
   }
 
-  customSort(event:any){
-    console.log(event)
-    if(event){
-    this.service.pagesort(this.page, this.rows, event.sortField ? event.sortField : "id", event.sortOrder === 1).subscribe((page:any)=>{     
-      this.items = page.lista;
-      this.total = page.total;
-    })
-  }
+  customSort(event: any) {
+    if (event) {
+      this.sort = event.sortField ? event.sortField : "id";
+      this.asc = event.sortOrder === 1;
+      this.service.pagesort(this.page, this.rows, this.sort , this.asc).subscribe((page: any) => {
+        this.items = page.lista;
+        this.total = page.total;
+      })
+    }
   }
 }
