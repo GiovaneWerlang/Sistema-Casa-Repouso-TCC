b99ef37fc5b8f99d08d4fc7d7afbe155076405a2
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-09-29 16:11:22 -0300

    adição sort por id padrão na paginação

--- src/main/java/br/edu/utfpr/crud/CrudRepository.java
+++ src/main/java/br/edu/utfpr/crud/CrudRepository.java
@@ -1,12 +1,14 @@
 package br.edu.utfpr.crud;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Sort;
+
 import java.util.List;
 
 public abstract class CrudRepository<T> implements PanacheRepository<T> {
 
     public List<T> pageList(int page, int size){
-        return findAll().page(page, size).list();
+        return findAll(Sort.by("id")).page(page, size).list();
     }
 
     public int pageCount(int page, int size){
