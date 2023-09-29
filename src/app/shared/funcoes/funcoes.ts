5e76c0ffd00aefe5768006f0985bc56e32214011
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-09-29 19:28:13 -0300

    funcoes

---
+++ src/app/shared/funcoes/funcoes.ts
@@ -0,0 +1,9 @@
+import { LabelValue } from "../labelvalue/labelvalue";
+
+export const Funcoes:LabelValue[] = [
+    {label:"Cuidador",value:"CUIDADOR"},
+    {label:"Enfermeiro",value:"ENFERMEIRO"},
+    {label:"Funcionário",value:"FUNCIONARIO"},
+    {label:"Médico",value:"MEDICO"},
+    {label:"Voluntário",value:"VOLUNTARIO"}
+];
