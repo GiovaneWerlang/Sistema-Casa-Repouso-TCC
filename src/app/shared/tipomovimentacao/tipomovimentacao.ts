3d399b4cb58ce252f5570e238614ed46860b6d92
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-10-01 16:09:34 -0300

    tipo movimentação

---
+++ src/app/shared/tipomovimentacao/tipomovimentacao.ts
@@ -0,0 +1,6 @@
+import { LabelValue } from "../labelvalue/labelvalue";
+
+export const TipoMovimentacao:LabelValue[] = [    
+    {label:"Entrada",value:"ENTRADA"},
+    {label:"Saída",value:"SAIDA"}
+]
