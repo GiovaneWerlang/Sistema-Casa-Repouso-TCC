095a4f4f6840bc5b39fe869b6e1e59d2413359c7
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-10-02 12:52:17 -0300

    paginação com sort

--- src/main/java/br/edu/utfpr/crud/CrudRepository.java
+++ src/main/java/br/edu/utfpr/crud/CrudRepository.java
@@ -11,6 +11,10 @@ public abstract class CrudRepository<T> implements PanacheRepository<T> {
         return findAll(Sort.by("id")).page(page, size).list();
     }
 
+    public List<T> pageListSort(int page, int size, String atributo, Sort.Direction direction){
+        return findAll(Sort.by(atributo, direction)).page(page, size).list();
+    }
+
     public int pageCount(int page, int size){
         return findAll().page(page, size).pageCount();
     }
