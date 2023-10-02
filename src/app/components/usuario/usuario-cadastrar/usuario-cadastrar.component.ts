23214798721ee3efd5acea273cb53ff33e1b0e18
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-10-02 18:16:28 -0300

    melhoria validação e mensagem usuário

--- src/app/components/usuario/usuario-cadastrar/usuario-cadastrar.component.ts
+++ src/app/components/usuario/usuario-cadastrar/usuario-cadastrar.component.ts
@@ -5,12 +5,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProfissionalService } from '../../profissional/service/profissional.service';
 import { LabelValue } from 'src/app/shared/labelvalue/labelvalue';
 import { Profissional } from '../../profissional/modelo/profissional';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-usuario-cadastrar',
   templateUrl: './usuario-cadastrar.component.html',
   styleUrls: ['./usuario-cadastrar.component.css'],
-  providers: [UsuarioService]
+  providers: [UsuarioService, MessageService]
 })
 export class UsuarioCadastrarComponent implements OnInit {
 
@@ -22,12 +23,13 @@ export class UsuarioCadastrarComponent implements OnInit {
     private usuarioService: UsuarioService,
     private profissionalService: ProfissionalService,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService
   ) {
     this.form = this.formBuilder.group({
       id: [null],
-      login: ['', Validators.compose([Validators.required, Validators.maxLength(50)])],
-      senha: ['', Validators.compose([Validators.required, Validators.minLength(6)])],      
+      login: [null, Validators.compose([Validators.required, Validators.maxLength(50)])],
+      senha: [null, Validators.compose([Validators.required, Validators.minLength(6)])],      
       profissional: [null, Validators.required]
     });
     this.carregarOpcoesProfissional();
@@ -42,16 +44,19 @@ export class UsuarioCadastrarComponent implements OnInit {
     })
   }
 
-  salvar() {
-    this.form.updateValueAndValidity()
-    console.log(this.form.valid)
-    // this.profissionalService.save(this.form.getRawValue()).subscribe((res) => {
-    //   if(res){
-    //     this.router.navigate(['/usuario/listar']);
-    //   }
-    // })
-    // this.limpar();
-    
+  salvar() {   
+    if(this.form.valid){
+      this.profissionalService.save(this.form.getRawValue()).subscribe((res) => {
+        if(res){
+          this.router.navigate(['/usuario/listar']);
+        }
+      })
+      this.limpar();
+    }else{
+      this.form.get('login')?.markAsTouched();
+      this.form.get('senha')?.markAsTouched();
+      this.messageService.add({ severity: 'warn', summary: 'Não foi possível salvar!', detail: 'Verifique os campos e tente novamente.' });
+    }
   }
 
   limpar() {
