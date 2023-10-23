7b8c1340ac002320e9e4e8c510e26be127ec6e93
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-10-23 20:32:50 -0300

    add sort to especialidade

--- src/app/components/especialidade/especialidade-listar/especialidade-listar.component.ts
+++ src/app/components/especialidade/especialidade-listar/especialidade-listar.component.ts
@@ -3,14 +3,16 @@ import { Especialidade } from '../modelo/especialidade';
 import { CrudTableComponent } from 'src/app/shared/crud-table/crud-table/crud-table.component';
 import { CrudService } from 'src/app/shared/crud-service/crud-service';
 import { EspecialidadeService } from '../service/especialidade.service';
+import { LabelValue } from 'src/app/shared/labelvalue/labelvalue';
 
 @Component({
   selector: 'app-especialidade-listar',
   templateUrl: './especialidade-listar.component.html',
   styleUrls: ['./especialidade-listar.component.css'],
-  providers: [  { provide: CrudService, useExisting: EspecialidadeService }]
+  providers: [{ provide: CrudService, useExisting: EspecialidadeService }]
 })
 export class EspecialidadeListarComponent extends CrudTableComponent<Especialidade> {
-  override cols:string[] = ["Id", "Nome"];
- 
+  override cols: LabelValue[] = [
+    { label: "Id", value: "id" }, { label: "Nome", value: "nome" }];
+
 }
