fc98f273f4789a810c2fe7cc206806f28182e1cd
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-10-23 20:51:43 -0300

    add sort to usuario

--- src/app/components/usuario/usuario-listar/usuario-listar.component.ts
+++ src/app/components/usuario/usuario-listar/usuario-listar.component.ts
@@ -21,6 +21,10 @@ export class UsuarioListarComponent {
   rows:number = 10;
   total:number = 0;
 
+  page: number = 0;
+  sort: string = "id";
+  asc: boolean = true;
+
   situacoes:LabelValue[] = Situacoes;
   funcoes:LabelValue[] = Funcoes;
 
@@ -49,7 +53,7 @@ export class UsuarioListarComponent {
   }
 
   carregarLista(page:number, size: number):void{
-    this.service.page(page, size).subscribe((page:any)=>{     
+    this.service.pagesort(page, size, this.sort, this.asc).subscribe((page: any) => {
       this.items = page.lista;
       this.total = page.total;
     })
@@ -58,5 +62,16 @@ export class UsuarioListarComponent {
   onPageChange(event:any){
     this.rows = event.rows;
     this.carregarLista(event.page, event.rows);
+  }
+
+  customSort(event: any) {
+    if (event) {
+      this.sort = event.sortField ? event.sortField : "id";
+      this.asc = event.sortOrder === 1;
+      this.service.pagesort(this.page, this.rows, this.sort , this.asc).subscribe((page: any) => {
+        this.items = page.lista;
+        this.total = page.total;
+      })
+    }
   }
 }
