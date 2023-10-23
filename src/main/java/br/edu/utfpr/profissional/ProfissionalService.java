a1b177bea218f35b283268de518d58c2b11d4572
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-10-23 20:35:52 -0300

    add sort to profissional

--- src/main/java/br/edu/utfpr/profissional/ProfissionalService.java
+++ src/main/java/br/edu/utfpr/profissional/ProfissionalService.java
@@ -203,4 +203,19 @@ public class ProfissionalService implements CrudService<ProfissionalDTO> {
         return Response.ok(pageDTO).build();
     }
 
+    public Response pageSort(int page, int size, String atributo, boolean asc){
+        if(page < 0 || size < 1){
+            return Response.status(422).build();
+        }
+        List<ProfissionalModel> lista = repository.pageListSort(page,size,atributo,asc ? Sort.Direction.Ascending : Sort.Direction.Descending);
+        if(lista.isEmpty()){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        PageDTO<ProfissionalModel> pageDTO = new PageDTO<>();
+        pageDTO.setLista(lista);
+        pageDTO.setPages(repository.pageCount(page,size));
+        pageDTO.setTotal(repository.pageTotal(page,size));
+        return Response.ok(pageDTO).build();
+    }
+
 }
