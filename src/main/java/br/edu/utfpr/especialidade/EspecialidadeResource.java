b40547e300e2d8aae35993732daac89703314a3f
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-10-23 20:34:43 -0300

    add sort to especialidade

--- src/main/java/br/edu/utfpr/especialidade/EspecialidadeResource.java
+++ src/main/java/br/edu/utfpr/especialidade/EspecialidadeResource.java
@@ -64,4 +64,10 @@ public class EspecialidadeResource implements CrudResource<EspecialidadeDTO> {
         return service.page(page,size);
     }
 
+    @GET
+    @Path("pagesort/{page}/{size}/{sort}/{asc}")
+    public Response page(@PathParam("page") int page, @PathParam("size") int size,@PathParam("sort") String sort,@PathParam("asc") boolean asc){
+        return service.pageSort(page,size,sort,asc);
+    }
+
 }
--- src/main/java/br/edu/utfpr/especialidade/EspecialidadeService.java
+++ src/main/java/br/edu/utfpr/especialidade/EspecialidadeService.java
@@ -101,4 +101,19 @@ public class EspecialidadeService implements CrudService<EspecialidadeDTO> {
         return Response.ok(pageDTO).build();
     }
 
+    public Response pageSort(int page, int size, String atributo, boolean asc){
+        if(page < 0 || size < 1){
+            return Response.status(422).build();
+        }
+        List<EspecialidadeModel> lista = repository.pageListSort(page,size,atributo,asc ? Sort.Direction.Ascending : Sort.Direction.Descending);
+        if(lista.isEmpty()){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        PageDTO<EspecialidadeModel> pageDTO = new PageDTO<>();
+        pageDTO.setLista(lista);
+        pageDTO.setPages(repository.pageCount(page,size));
+        pageDTO.setTotal(repository.pageTotal(page,size));
+        return Response.ok(pageDTO).build();
+    }
+
 }
