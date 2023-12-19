82c897d8a4ab4858523729637e00c036af496931
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-12-18 21:43:04 -0300

    dashboard sem informação

--- src/app/components/login/autenticacaointerceptor.service.ts
+++ src/app/components/login/autenticacaointerceptor.service.ts
@@ -6,15 +6,19 @@ export class AutenticacaoInterceptorService implements HttpInterceptor {
     headers:HttpHeaders | undefined;
     
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let dados = localStorage.getItem('dadosUsuario');
-        let token = '';
-        if(dados){
-            token = JSON.parse(dados)._token;
+        if(!req.url.includes('login')){
+            let dados = localStorage.getItem('dadosUsuario');
+            let token = '';
+            if(dados){
+                token = JSON.parse(dados)._token;
+            }
+            const modifiedReq = req.clone({
+                headers: req.headers.append('Authorization','Bearer ' + token)
+            })
+            return next.handle(modifiedReq);
         }
-        const modifiedReq = req.clone({
-            headers: req.headers.append('Authorization','Bearer ' + token)
-        })
-        return next.handle(modifiedReq);
+
+        return next.handle(req);
     }
 
 }
