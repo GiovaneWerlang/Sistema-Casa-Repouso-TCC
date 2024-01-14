face1d32579ced57e29828947a72fa04f6e6c399
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2024-01-13 22:19:12 -0300

    processo de envio de email e whatsapp

--- src/main/java/br/edu/utfpr/atividadesresidente/AtividadeResidenteScheduler.java
+++ src/main/java/br/edu/utfpr/atividadesresidente/AtividadeResidenteScheduler.java
@@ -7,7 +7,9 @@ import br.edu.utfpr.atividadesresidente.processa.ProcessaAtividade;
 import br.edu.utfpr.atividadesresidente.processa.ProcessaAtividadeConsulta;
 import br.edu.utfpr.atividadesresidente.processa.ProcessaAtividadeExame;
 import br.edu.utfpr.atividadesresidente.processa.ProcessaAtividadeMedicamento;
+import br.edu.utfpr.email.EnvioEmail;
 import br.edu.utfpr.profissional.ProfissionalService;
+import br.edu.utfpr.whatsapp.EnvioWhatsapp;
 import io.quarkus.scheduler.Scheduled;
 import io.quarkus.scheduler.ScheduledExecution;
 
@@ -26,38 +28,51 @@ public class AtividadeResidenteScheduler {
 
     private ProfissionalService profissionalService;
 
+    private EnvioEmail envioEmail;
+    private EnvioWhatsapp envioWhatsapp;
+
     @Inject
     public AtividadeResidenteScheduler(AtividadeConsultaResidenteRepository atividadeConsultaResidenteRepository,
                                        AtividadeExameResidenteRepository atividadeExameResidenteRepository,
                                        AtividadeMedicamentoResidenteRepository atividadeMedicamentoResidenteRepository,
-                                       ProfissionalService profissionalService) {
+                                       ProfissionalService profissionalService,
+                                       EnvioEmail envioEmail,
+                                       EnvioWhatsapp envioWhatsapp
+    ) {
         this.atividadeConsultaResidenteRepository = atividadeConsultaResidenteRepository;
         this.atividadeExameResidenteRepository = atividadeExameResidenteRepository;
         this.atividadeMedicamentoResidenteRepository = atividadeMedicamentoResidenteRepository;
         this.profissionalService = profissionalService;
+        this.envioEmail = envioEmail;
+        this.envioWhatsapp = envioWhatsapp;
     }
 
-    @Scheduled(every="1h")
+    //@Scheduled(every="1h")
+    @Scheduled(every="2m")
     void cronJob(ScheduledExecution execution) {
-        processarAtividades(buscarEmails());
+        processarAtividades(buscarEmails(), buscarTelefones());
     }
 
     public List<String> buscarEmails(){
         return profissionalService.buscarEmails();
     }
 
+    public List<String> buscarTelefones(){
+        return profissionalService.buscarTelefones();
+    }
+
     @Transactional
-    public void processarAtividades( List<String> emails){
+    public void processarAtividades(List<String> emails, List<String> telefones){
         ProcessaAtividade processaAtividade;
 
-        processaAtividade = new ProcessaAtividadeConsulta(atividadeConsultaResidenteRepository);
-        processaAtividade.processarLista(emails);
+        processaAtividade = new ProcessaAtividadeConsulta(atividadeConsultaResidenteRepository, envioEmail, envioWhatsapp);
+        processaAtividade.processarLista(emails, telefones);
 
-        processaAtividade = new ProcessaAtividadeExame(atividadeExameResidenteRepository);
-        processaAtividade.processarLista(emails);
+        processaAtividade = new ProcessaAtividadeExame(atividadeExameResidenteRepository, envioEmail, envioWhatsapp);
+        processaAtividade.processarLista(emails, telefones);
 
-        processaAtividade = new ProcessaAtividadeMedicamento(atividadeMedicamentoResidenteRepository);
-        processaAtividade.processarLista(emails);
+        processaAtividade = new ProcessaAtividadeMedicamento(atividadeMedicamentoResidenteRepository, envioEmail, envioWhatsapp);
+        processaAtividade.processarLista(emails, telefones);
     }
 
 }
