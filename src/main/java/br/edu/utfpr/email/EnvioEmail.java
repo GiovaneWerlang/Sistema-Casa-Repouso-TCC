5c39882457829d02001d1d4d0c2da3d8346f30fc
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2024-01-13 22:25:42 -0300

    processo de envio de email e whatsapp

--- src/main/java/br/edu/utfpr/email/EnvioEmail.java
+++ src/main/java/br/edu/utfpr/email/EnvioEmail.java
@@ -15,6 +15,9 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class EnvioEmail {
 
+    @ConfigProperty(name = "email.habilitar", defaultValue="Não configurado!")
+    boolean habilitar;
+
     @ConfigProperty(name = "email.login", defaultValue="Não configurado!")
     String login;
 
@@ -23,32 +26,35 @@ public class EnvioEmail {
 
     public void enviar(String emailProfissional, String subject, String texto) {
 
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp-mail.outlook.com");
-        props.put("mail.smtp.port", "587");
+        if(habilitar) {
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp-mail.outlook.com");
+            props.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(props,
-                new jakarta.mail.Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(login, senha);
-                    }
-                });
+            Session session = Session.getInstance(props,
+                    new jakarta.mail.Authenticator() {
+                        @Override
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(login, senha);
+                        }
+                    });
 
-        try {
+            try {
 
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(login));
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(emailProfissional));
-            message.setSubject(subject);
-            message.setText(texto);
-            Transport.send(message);
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(login));
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(emailProfissional));
+                message.setSubject(subject);
+                message.setText(texto);
+                Transport.send(message);
 
-        } catch (MessagingException e) {
-            e.printStackTrace();
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println(habilitar);
 
     }
 
--- src/main/java/br/edu/utfpr/whatsapp/EnvioWhatsapp.java
+++ src/main/java/br/edu/utfpr/whatsapp/EnvioWhatsapp.java
@@ -13,6 +13,9 @@ import java.net.http.HttpResponse;
 @ApplicationScoped
 public class EnvioWhatsapp {
 
+    @ConfigProperty(name = "whatsapp.habilitar", defaultValue="Não configurado!")
+    boolean habilitar;
+
     @ConfigProperty(name = "whatsapp.numeroid", defaultValue="Não configurado!")
     String numeroid;
 
@@ -21,18 +24,20 @@ public class EnvioWhatsapp {
 
     public void enviar(String telefoneProfissional, String texto) {
 
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("https://graph.facebook.com/v13.0/"+numeroid+"/messages"))
-                    .header("Authorization", "Bearer "+token)
-                    .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString("{ \"messaging_product\": \"whatsapp\", \"recipient_type\": \"individual\", \"to\": \" 55"+telefoneProfissional+"\", \"type\": \"text\", \"text\": { \"preview_url\": false, \"body\": \"" + texto + "\" } }"))
-                    .build();
-            HttpClient http = HttpClient.newHttpClient();
-            HttpResponse<String> response = http.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (URISyntaxException | IOException | InterruptedException e) {
-            e.printStackTrace();
+        if(habilitar) {
+            try {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI("https://graph.facebook.com/v13.0/" + numeroid + "/messages"))
+                        .header("Authorization", "Bearer " + token)
+                        .header("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString("{ \"messaging_product\": \"whatsapp\", \"recipient_type\": \"individual\", \"to\": \" 55" + telefoneProfissional + "\", \"type\": \"text\", \"text\": { \"preview_url\": false, \"body\": \"" + texto + "\" } }"))
+                        .build();
+                HttpClient http = HttpClient.newHttpClient();
+                HttpResponse<String> response = http.send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (URISyntaxException | IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
+        System.out.println(habilitar);
     }
 }
