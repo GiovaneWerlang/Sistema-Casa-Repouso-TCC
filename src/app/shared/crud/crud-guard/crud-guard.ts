1614fe7134d44cb2b42facd2bcbbb11c11ae75c1
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2024-02-03 15:29:28 -0300

    alteracao de candeactivate para fn

--- src/app/shared/crud/crud-guard/crud-guard.ts
+++ src/app/shared/crud/crud-guard/crud-guard.ts
@@ -1,47 +1,40 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, CanDeactivateFn, RouterStateSnapshot } from "@angular/router";
 import { ConfirmationService } from "primeng/api";
 import { Observable, Observer } from "rxjs";
 
-@Injectable({
-    providedIn: 'root'
-})
-export class CrudGuard implements CanDeactivate<any> {
+export const CrudGuard: CanDeactivateFn<any> = (
+    component: any,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState: RouterStateSnapshot
+) => {
 
-    constructor(private confirmationService: ConfirmationService) {
+    const confirmationService = inject(ConfirmationService);
+
+    if (!component?.form?.dirty) {
+        return true;
     }
 
-    canDeactivate(
-        component: any,
-        currentRoute: ActivatedRouteSnapshot,
-        currentState: RouterStateSnapshot,
-        nextState: RouterStateSnapshot
-    ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        console.log(component?.form?.dirty)
+    return new Observable((observer: Observer<boolean>) => {
 
-        if (!component?.form?.dirty) {
-            return true;
-        }
+        confirmationService.confirm({
+            message: 'Tem certeza que descartar as alterações?',
+            header: 'Confirmação',
+            icon: 'pi pi-info-circle',
+            acceptButtonStyleClass: "p-button-danger p-button-text",
+            rejectButtonStyleClass: "p-button-text p-button-text",
+            acceptIcon: "none",
+            rejectIcon: "none",
+            accept: () => {
+                observer.next(true);
+                observer.complete();
+            },
+            reject: () => {
+                observer.next(false);
+                observer.complete();
+            }
+        });
+    });
 
-        return new Observable((observer: Observer<boolean>) => {
-            
-            this.confirmationService.confirm({
-                message: 'Tem certeza que descartar as alterações?',
-                header: 'Confirmação',
-                icon: 'pi pi-info-circle',
-                acceptButtonStyleClass: "p-button-danger p-button-text",
-                rejectButtonStyleClass: "p-button-text p-button-text",
-                acceptIcon: "none",
-                rejectIcon: "none",
-                accept: () => {
-                    observer.next(true);
-                    observer.complete();
-                },
-                reject: () => {
-                    observer.next(false);
-                    observer.complete();
-                }
-            });
-        });        
-    }
 }
