91ea7dd42d6848babe5f57ca20f03501b7902b24
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2024-02-03 15:29:19 -0300

    alteracao de canactivate para fn

--- src/app/components/login/autenticacao.guard.ts
+++ src/app/components/login/autenticacao.guard.ts
@@ -1,29 +1,27 @@
 import { AutenticacaoService } from './service/autenticacao.service';
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable, map } from "rxjs";
+import { inject } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
+import { map } from "rxjs";
 
-@Injectable({providedIn: 'root'})
-export class AutenticacaoGuard implements CanActivate {
+export const AutenticacaoGuard: CanActivateFn = (
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+) => {
+    const autenticacaoService = inject(AutenticacaoService);
+    const router = inject(Router);
 
-    constructor(private autenticacaoService:AutenticacaoService, private router:Router){
-    }
-
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {        
-        return this.autenticacaoService.dadoUsuario
+    return autenticacaoService.dadoUsuario
         .pipe(
             map((usuario) => {
                 const autenticado = !!usuario;
-                if(autenticado){
-                    if(state.url === '/login'){
-                        return this.router.createUrlTree(['/home']);
-                    }else{
+                if (autenticado) {
+                    if (state.url === '/login') {
+                        return router.createUrlTree(['/home']);
+                    } else {
                         return true;
                     }
                 }
-                return this.router.createUrlTree(['/login']);
+                return router.createUrlTree(['/login']);
             })
         );
-    }
-
 }
