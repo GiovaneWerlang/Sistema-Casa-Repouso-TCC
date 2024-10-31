006b12f89c66497a3626f27149942636c93a87a9
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2024-10-31 19:55:22 -0300

    ajuste dto

--- src/main/java/br/edu/utfpr/dashboard/AtividadeDashDTO.java
+++ src/main/java/br/edu/utfpr/dashboard/AtividadeDashDTO.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class AtividadeDashDTO {
 
-    private long id;
+    private Long id;
     private String descricao;
     private LocalDateTime dataHora;
     private String tipo;
